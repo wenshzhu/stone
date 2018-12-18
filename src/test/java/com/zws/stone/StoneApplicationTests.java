292c2a3c62c57513a4063740ea4febc3e05b1d1e
wensh.zhu <zhuwensheng@mi-ya.com.cn>
2018-12-18 21:44:33 +0800

    init

---
+++ src/test/java/com/zws/stone/StoneApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zws.stone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
